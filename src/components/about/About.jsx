ca7718f8cce08c917c31ed78f04ee4ca27afd925
Deepen Timalsina <deepentimalsina05@gmail.com>
2024-03-26 22:23:21 +0545

    about updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,15 +35,7 @@ const About = () => {
             </article>
           </div>
           <p>
-            {`Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.`}
+            {`I’m a software developer with experience at Prologic Solutions, where I developed various web applications. My work ranges from simple websites to complex web apps, always focusing on user experience and quality. I’m skilled in multiple programming languages and always ready to learn new technologies.`}
           </p>
           <a href={"contact"} className={"btn btn-primary"}>
            {` Let's Talk`}
